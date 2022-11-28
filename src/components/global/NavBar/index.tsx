139b38203b877b581b7e8517700bb1999ce76b41
felipersan <feliperodrigues.san@gmail.com>
2022-11-28 18:29:10 -0300

    att - components/sidebar

--- src/components/global/NavBar/index.tsx
+++ src/components/global/NavBar/index.tsx
@@ -1,24 +1,44 @@
-import { SearchIcon, TrendIcon } from "../../../../public/assets";
+import { AddIcon, SearchIcon, TrendIcon } from "../../../../public/assets";
 
 export default function NavBar() {
   const menu = [
-    { icon: <SearchIcon color={"#6EEB83"} size={40} />, title: "Search" },{ icon: <TrendIcon color={"#6EEB83"} size={40} />, title: "Trending" }
+    { icon: <SearchIcon color={"#6EEB83"} size={30} />, title: "Search" },
+    { icon: <TrendIcon color={"#6EEB83"} size={30} />, title: "Trending" },
   ];
   return (
-    <main className="min-h-screen w-40 border-solid border-r-2 border-emphasis-primary flex items-center justify-center flex-col gap-y-10">
-      <div className="bg-emphasis-primary h-20 w-20 rounded-full flex items-center justify-center">
-        <span className="text-4xl text-white">P</span>
+    <main className="min-h-screen w-24 border-solid border-r-2 border-emphasis-primary flex items-center justify-center flex-col gap-y-10">
+      <div className="bg-emphasis-primary h-16 w-16 rounded-full flex items-center justify-center">
+        <a href="/">
+          <span className="text-4xl text-white">P</span>
+        </a>
       </div>
       <nav>
         <ul className="flex flex-col gap-y-10">
           {menu.map((row: any, key: number) => (
             <li key={key}>
-              <a className="cursor-pointer flex flex-col items-center" href={`/${row.title}`}>
-                {row.icon}
-                <p className="text-white">{row.title}</p>
+              <a
+                className="cursor-pointer flex flex-col items-center"
+                href={`/${row?.title?.toLowerCase()}`}
+              >
+                {row?.icon}
+                <p className="text-white">{row?.title}</p>
               </a>
             </li>
           ))}
+        </ul>
+      </nav>
+      <nav className="mt-20">
+        <ul className="flex flex-col gap-y-10">
+          <li key={"create component"}>
+            <a
+              className="cursor-pointer flex flex-col items-center"
+              href={`/create`}
+            >
+              <AddIcon color={"#6EEB83"} size={30} />
+
+              <p className="text-white">Create</p>
+            </a>
+          </li>
         </ul>
       </nav>
     </main>
