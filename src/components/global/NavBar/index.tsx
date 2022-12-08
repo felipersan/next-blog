0adee5b1df6ec9bc7e1e3060922a0ac3967999ed
felipersan <feliperodrigues.san@gmail.com>
2022-12-08 00:49:32 -0300

    update file

--- src/components/global/NavBar/index.tsx
+++ src/components/global/NavBar/index.tsx
@@ -1,46 +1,47 @@
-import { AddIcon, SearchIcon, TrendIcon } from "../../../../public/assets";
+import Link from 'next/link'
+import { AddIcon, SearchIcon, TrendIcon } from '../../../../public/assets'
 
 export default function NavBar() {
   const menu = [
-    { icon: <SearchIcon color={"#6EEB83"} size={30} />, title: "Search" },
-    { icon: <TrendIcon color={"#6EEB83"} size={30} />, title: "Trending" },
-  ];
+    { icon: <SearchIcon color={'#6EEB83'} size={30} />, title: 'Search' },
+    { icon: <TrendIcon color={'#6EEB83'} size={30} />, title: 'Trending' }
+  ]
   return (
     <main className="min-h-screen w-24 border-solid border-r-2 border-emphasis-primary flex items-center justify-center flex-col gap-y-10">
       <div className="bg-emphasis-primary h-16 w-16 rounded-full flex items-center justify-center">
-        <a href="/">
+        <Link href="/">
           <span className="text-4xl text-white">P</span>
-        </a>
+        </Link>
       </div>
       <nav>
         <ul className="flex flex-col gap-y-10">
           {menu.map((row: any, key: number) => (
             <li key={key}>
-              <a
+              <Link
                 className="cursor-pointer flex flex-col items-center"
                 href={`/${row?.title?.toLowerCase()}`}
               >
                 {row?.icon}
                 <p className="text-white">{row?.title}</p>
-              </a>
+              </Link>
             </li>
           ))}
         </ul>
       </nav>
       <nav className="mt-20">
         <ul className="flex flex-col gap-y-10">
-          <li key={"create component"}>
-            <a
+          <li key={'create component'}>
+            <Link
               className="cursor-pointer flex flex-col items-center"
               href={`/create`}
             >
-              <AddIcon color={"#6EEB83"} size={30} />
+              <AddIcon color={'#6EEB83'} size={30} />
 
               <p className="text-white">Create</p>
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
     </main>
-  );
+  )
 }
