a46e2ee6c0341ee52f174413605d6a7eac62c6b1
felipersan <feliperodrigues.san@gmail.com>
2023-01-07 21:48:15 -0300

    update files

--- src/pages/criar/index.tsx
+++ src/pages/criar/index.tsx
@@ -1,3 +1,15 @@
-export default function CreatePost(){
-    return (<p>Teste</p>)
+import { useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+
+
+export default function CreatePost() {
+  const [editorState, setEditorState] = useState<any>();
+  return (
+      <Editor
+          editorState={editorState}
+          wrapperClassName="demo-wrapper"
+          editorClassName="demo-editor"
+          onEditorStateChange={()=>{}}
+        />
+  );
 }
